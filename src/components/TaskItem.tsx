6d795643033b248e3670a57cc3ca5e888bd4a612
Yosef <mercytv932@gmail.com>
2026-08-26 16:17:43 -0400

    done with the onChnage and button

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -1,3 +1,5 @@
+import type { TaskStatus } from "./TaskList";
+
 // types/index.ts
 export interface TaskItemProps {
   task: Task;
@@ -6,7 +8,30 @@ export interface TaskItemProps {
 }
 
 function TaskItem({ task, onStatusChange, onDelete }: TaskItemProps) {
+  function handleDelete() {
+    onDelete(task.id);
+  }
 
+  function handleStatus(event: React.ChangeEvent<HTMLSelectElement>) {
+    const newStatus = event.target.value as TaskStatus;
+
+    onStatusChange(task.id, newStatus);
+  }
+  return (
+    <div>
+      <h3>title: {task.title}</h3>
+      <h4>Description: {task.description}</h4>
+      <h5>Status: {task.status}</h5>
+      <h5>Priority: {task.priority}</h5>
+      <p>Due: {task.dueDate}</p>
+      <select onChange={handleStatus}>
+        <option value="pending">Pending</option>
+        <option value="in-progress">In-Progress</option>
+        <option value="completed">Completed</option>
+      </select>
+      <button onClick={handleDelete}></button>
+    </div>
+  );
 }
 
 export default TaskItem;
