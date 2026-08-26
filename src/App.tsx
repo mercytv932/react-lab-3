0708e6a72df0ef7f90026211ca2ef68b723ae591
Yosef <mercytv932@gmail.com>
2026-08-26 17:00:55 -0400

    handlestatus done

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import { useState } from "react";
 
 import { tasks } from "./data/Data";
-import TaskList from "./components/TaskList";
+import TaskList, { type TaskStatus } from "./components/TaskList";
 
 function App() {
   const [taskList, setTaskList] = useState(tasks);
 
-  function handleDelete(taskId: string) {
+  function handleDeleteBtn(taskId: string) {
     const updatedTasks = taskList.filter((task) => task.id !== taskId);
 
     setTaskList(updatedTasks);
   }
+
+  function handleStatusChange(taskId: string, newStatus: TaskStatus) {
+    const tasksAfterStatusChange = taskList.map((task) =>
+      task.id === taskId ? { ...task, status: newStatus } : task,
+    );
+
+    setTaskList(tasksAfterStatusChange);
+  }
+
+  return (
+    <TaskList
+      tasks={taskList}
+      onDelete={handleDeleteBtn}
+      onStatusChange={handleStatusChange}
+    />
+  );
 }
 
 export default App;
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -30,6 +30,7 @@ function TaskList({ tasks, onStatusChange, onDelete }: TaskListProps) {
           task={task}
           onStatusChange={onStatusChange}
           onDelete={onDelete}
+          // tasks = {TaskList}
         />
       ))}
     </div>
